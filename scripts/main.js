51285e047681edf004e5f0ceb671c4ac03e8189a
Eugene <evgnkuchma@gmail.com>
2020-03-17 18:20:47 +0200

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,6 @@
+/* eslint-disable max-len */
 'use strict';
+
+document.getElementsByClassName('menu__btn')[0].onclick = function() {
+  document.getElementsByClassName('nav__list')[0].classList.toggle('nav__list-show');
+};
